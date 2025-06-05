7966c79b1a9c2a3979c460e982f1ab7bfcf88d84
Alejandro Escobar <alejandroescobar.98@gmail.com>
2025-06-05 18:00:06 +0200

    Images working on local - not github pages

--- js/Game.js
+++ js/Game.js
@@ -51,7 +51,7 @@ export class Game {
 		await Swal.fire({
 			title: `${player.name} ha tirado el dado`,
 			text: `Sacó un ${roll}. Nueva posición: ${player.position}`,
-			imageUrl: "media/img/dice/dice-" + roll,
+			imageUrl: "media/img/dice/dice" + roll + ".png",
 			imageWidth: 100,
 			imageHeight: 100
 		});
